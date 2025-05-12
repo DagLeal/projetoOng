b589e4fbdfbdaa9a8999af43b013c4531e5bb3eb
DagLeal <dagnerleal.job@gmail.com>
2025-05-12 16:10:44 -0300

    Ajustes

--- core/static/js/header.js
+++ core/static/js/header.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const header = document.getElementById('main-header');
   const logoImg = document.getElementById('logo-img');
+  const main = document.getElementById('main-content');
 
   window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
       header.classList.add('shrink');
       logoImg.src = "/static/core/img/logo-pequena.png";
+      main.style.paddingTop = "5.5vh";
     } else {
       header.classList.remove('shrink');
       logoImg.src = "/static/core/img/logo-grande.png";
+      main.style.paddingTop = "17.5vh";
     }
   });
 });
