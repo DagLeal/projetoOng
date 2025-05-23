c57f06d2bce77b15e02810e610ed61ff572b1f9d
DagLeal <dagnerleal.job@gmail.com>
2025-05-23 14:54:18 -0300

    Colocando o footer em todas as páginas e ajustes header. (Acabei desconfigurando um pouco a página de contatos)

--- core/static/js/header.js
+++ core/static/js/header.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
       header.classList.add('shrink');
-      logoImg.src = "/static/core/img/logo-pequena.png";
+      logoImg.src = "/static/img/logo-pequena.png";
       main.style.paddingTop = "5.5vh";
     } else {
       header.classList.remove('shrink');
-      logoImg.src = "/static/core/img/logo-grande.png";
+      logoImg.src = "/static/img/logo-grande.png";
       main.style.paddingTop = "17.5vh";
     }
   });
