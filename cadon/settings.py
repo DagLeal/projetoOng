48be189977a6d172a2587458c9b9c0a68a4eb302
DagLeal <dagnerleal.job@gmail.com>
2025-06-07 14:31:51 -0300

    Pequenos ajustes

--- cadon/settings.py
+++ cadon/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-bi64li4y8p!1a+p96w@%@!#we5cr!r#yho3%pqttd%gn3bt&&v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "projetoong.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
