876c38dd6c0aac2be89e289a34a3c7c14cfd686f
DagLeal <dagnerleal.job@gmail.com>
2025-06-07 15:16:05 -0300

    Pequenos ajustes

--- cadon/settings.py
+++ cadon/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),  # Points to your static folder
 ]
 
 # Default primary key field type
